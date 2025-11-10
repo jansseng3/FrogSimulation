e0015bff39a526234a78d9c1a4700160bd99535b
Janssen Gallenero <jansseng3@nycstudents.net>
2025-11-10 12:27:02 -0500

    added methods

--- src/FrogSimulation.java
+++ src/FrogSimulation.java
@@ -1,4 +1,30 @@
-package PACKAGE_NAME;
-
 public class FrogSimulation {
+    private int goalDistance;
+    private int maxHops;
+
+    public FrogSimulation(int dist, int numHops) {
+        goalDistance = dist;
+        maxHops = numHops;
+
+        //Precondition: dist > 0; numHops > 0
+    }
+
+    private int hopDistance() {
+//Returns an integer representing the distance, in inches, to be moved when the frog hops.
+
+    }
+
+    public boolean simulate() {
+//Simulates a frog attempting to reach the goal as described in part (a).
+//* Returns true if the frog successfully reached or passed the goal during the simulation;
+//* false otherwise.
+
+    }
+
+    public double runSimulations(int num) {
+//Runs num simulations and returns the proportion of simulations in which the frog
+//* successfully reached or passed the goal.
+//* Precondition: num > 0
+    }
+
 }
